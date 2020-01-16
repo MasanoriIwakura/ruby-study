5cfbc3b0a139c291299317e306e55b7855a6d2c9
MasanoriIwakura <hagobutler@yahoo.co.jp>
2020-01-16 15:10:50 +0900

    Add fizz_buzz test

--- cherry/lib/fizz_buzz.rb
+++ cherry/lib/fizz_buzz.rb
@@ -1,23 +1,11 @@
 def fizz_buzz(num)
-    if num % 15 == 0
-        puts 'FizzBuzz'
-    elsif num % 3 == 0
-        puts 'Fizz'
-    elsif num % 5 == 0
-        puts 'Buzz'
-    else
-        puts num
-    end
+  if num % 15 == 0
+      'FizzBuzz'
+  elsif num % 3 == 0
+      'Fizz'
+  elsif num % 5 == 0
+      'Buzz'
+  else
+      num.to_s
+  end
 end
-
-def greeting
-    'Hello'
-end
-
-(1..20).each{|num|
-    fizz_buzz(num)
-}
-
-g = greeting
-puts g
-puts g
